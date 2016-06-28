4bc46dd3f7c4df87b64201c8c10d40613d301bd1
Tantax <303340932@qq.com>
2016-06-29 00:49:55 +0800

    1.WCF客户端
    调用服务端的ShowName方法

---
+++ WCFClient/WCFTest.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+// 引用WCF命名空间
+using WCFClient.WCFService;
+
+namespace WCFClient
+{
+    public partial class WCFTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnClick(object sender, EventArgs e)
+        {         
+            WCFService.UserClient user = new UserClient();
+            Response.Write(user.ShowName(this.txtName.Text));
+        }
+    }
+}
